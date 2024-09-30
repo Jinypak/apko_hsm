ee451afdadcb8d92d3bdef8d253fcf22e1c6f6ce
jiny3360 <jiny3360@naver.com>
2024-09-30 16:46:46 +0900

    20240930 - # fixed build error

--- app/(main)/error/page.tsx
+++ app/(main)/error/page.tsx
@@ -1,19 +1,6 @@
 'use client'; // Error boundaries must be Client Components
 
-import { useEffect } from 'react';
-
-export default function Error({
-  error,
-  reset,
-}: {
-  error: Error & { digest?: string };
-  reset: () => void;
-}) {
-  useEffect(() => {
-    // Log the error to an error reporting service
-    console.error(error);
-  }, [error]);
-
+export default function Error() {
   return (
     <div className="w-full ">
       <h2>Error page</h2>
