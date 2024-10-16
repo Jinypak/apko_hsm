dd0cbaa7d1b3f2860f6a9b6d99fb9699ab4ce00e
jiny3360 <jiny3360@naver.com>
2024-10-16 17:41:36 +0900

    20241016 #1 - Add History Page exam image

--- app/(main)/manage/history/page.tsx
+++ app/(main)/manage/history/page.tsx
@@ -1,9 +1,21 @@
+import Image from 'next/image';
 import React from 'react';
 
 type Props = {};
 
 const HistoryPage = (props: Props) => {
-  return <div>HistoryPage</div>;
+  return (
+    <div>
+      HistoryPage
+      <p>고객사 히스토리 작성 예정</p>
+      <Image
+        src="/hsm/history.png"
+        alt="Notion SAMPLE"
+        width={1280}
+        height={1080}
+      />
+    </div>
+  );
 };
 
 export default HistoryPage;
